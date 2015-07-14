a52a972484ee505e680fdf142d3379af5ee7df22
redcode <manuel@redcodesoftware.com>
2015-07-14 23:33:13 +0200

    Updated ALSound to the lastest version.

--- Sources/ALSound.m
+++ Sources/ALSound.m
@@ -3,14 +3,30 @@
   _______ ___/ /______ ___/ /__
  / __/ -_) _  / __/ _ \ _  / -_)
 /_/  \__/\_,_/\__/\___/_,_/\__/
-Copyright © 2013 Manuel Sainz de Baranda y Goñi.
-Released under the terms of the GNU Lesser General Public License v3. */
+Copyright © 2013-2015 Manuel Sainz de Baranda y Goñi.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
 
 #import "ALSound.h"
 #import <AudioToolbox/AudioToolbox.h>
 
-#define AUDIO_FORMAT_FLAGS \
-	(kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsSignedInteger)
+#define AUDIO_FORMAT_FLAGS (kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsSignedInteger)
 
 static NSMutableDictionary* sounds_  = nil;
 static ALCcontext*	    context_ = NULL;
@@ -23,9 +39,9 @@ static ALCcontext*	    context_ = NULL;
 		{
 		ALSound *sound;
 
-		//-------------------------------------------------------------.
-		// Si el sonido está cacheado no es necesario volver a crearlo |
-		//-------------------------------------------------------------'
+		//-----------------------------------------------------------------------.
+		// If the sound is cached it's not necessary to create another instance. |
+		//-----------------------------------------------------------------------'
 		if (sounds_ && (sound = [sounds_ objectForKey: fileName])) return sound;
 
 		ExtAudioFileRef		    file;
@@ -35,53 +51,52 @@ static ALCcontext*	    context_ = NULL;
 		AudioStreamBasicDescription description;
 		uint16_t*		    buffer;
 
-		//------------------------------.
-		// Abrimos el archivo de sonido |
-		//------------------------------'
-		if (noErr != ExtAudioFileOpenURL
-			((CFURLRef)[[[NSURL alloc] initFileURLWithPath: [[NSBundle mainBundle]
-				pathForResource: [fileName stringByDeletingPathExtension]
-				ofType:		 [fileName pathExtension]]]
-			 autorelease],
-			 &file)
+		//----------------------.
+		// Open the sound file. |
+		//----------------------'
+		if (	noErr != ExtAudioFileOpenURL
+				((CFURLRef)[[[NSURL alloc] initFileURLWithPath: [[NSBundle mainBundle]
+					pathForResource: [fileName stringByDeletingPathExtension]
+					ofType:		 [fileName pathExtension]]]
+				 autorelease],
+				 &file)
 		)
 			return nil;
 
-		//--------------------------------------------------------------.
-		// Obtenemos el número de samples y las propiedades del formato |
-		//--------------------------------------------------------------'
+		//-----------------------------------------------------------.
+		// Obtain the number of samples and the format's properties. |
+		//-----------------------------------------------------------'
 		if (	noErr != ExtAudioFileGetProperty(file, kExtAudioFileProperty_FileLengthFrames, &size,		 &frameCount) ||
 			noErr != ExtAudioFileGetProperty(file, kExtAudioFileProperty_FileDataFormat,   &descriptionSize, &description)
 		)
 			goto close_file_and_return_nil;
 
-		//-----------------------------------------------------.
-		// Si el formato no es PCM, ajustamos las propiedades  |
-		// para que los samples sean convertidos a PCM al leer |
-		//-----------------------------------------------------'
+		//---------------------------------------------------------------------------------------.
+		// If the format is not PCM, adjust properties to convert the samples to PCM at reading. |
+		//---------------------------------------------------------------------------------------'
 		if (description.mFormatID != kAudioFormatLinearPCM || description.mFormatFlags != AUDIO_FORMAT_FLAGS)
 			{
-			description.mFormatID = kAudioFormatLinearPCM;
+			description.mFormatID	 = kAudioFormatLinearPCM;
 			description.mFormatFlags = AUDIO_FORMAT_FLAGS;
 
 			ExtAudioFileSetProperty(file, kExtAudioFileProperty_ClientDataFormat, descriptionSize, &description);
 			}
 
-		//---------------------------------------------.
-		// Creamos el buffer en dónde leer los samples |
-		//---------------------------------------------'
+		//----------------------------------------------.
+		// Create the buffer where to read the samples. |
+		//----------------------------------------------'
 		if ((buffer = malloc((size_t)(frameCount * description.mBytesPerFrame))) == NULL)
 			goto close_file_and_return_nil;
 
 		AudioBufferList bufferList;
-		bufferList.mNumberBuffers = 1;
+		bufferList.mNumberBuffers	       = 1;
 		bufferList.mBuffers[0].mNumberChannels = description.mChannelsPerFrame;
 		bufferList.mBuffers[0].mDataByteSize   = description.mBytesPerFrame * (UInt32)frameCount;
 		bufferList.mBuffers[0].mData	       = buffer;
 
-		//-----------------------------------------------------------------.
-		// Posicionamos la lectura al principio y leemos todos los samples |
-		//-----------------------------------------------------------------'
+		//---------------------------------------------------------.
+		// Seek to the beginning of file and read all the samples. |
+		//---------------------------------------------------------'
 		size = (UInt32)frameCount;
 
 		if (	noErr != ExtAudioFileSeek(file, 0) ||
@@ -89,34 +104,34 @@ static ALCcontext*	    context_ = NULL;
 		)
 			goto free_buffer_close_file_and_return_nil;
 
-		//---------------------.
-		// Cerramos el archivo |
-		//---------------------'
+		//-----------------------.
+		// Close the sound file. |
+		//-----------------------'
 		ExtAudioFileDispose(file);
 
 		//----------------------.
-		// Creamos la instancia |
+		// Create the instance. |
 		//----------------------'
 		if ((sound = [[self alloc] init]))
 			{
 			sound->_buffer = buffer;
 
-			//-----------------------------------.
-			// Si todavía no existía ninguna.... |
-			//-----------------------------------'
+			//------------------------------------------.
+			// If there was not any instance created... |
+			//------------------------------------------'
 			if (!sounds_)
 				{
-				//---------------------------------.
-				// Creamos el diccionario de caché |
-				//---------------------------------'
+				//------------------------------.
+				// Create the cache dictionary. |
+				//------------------------------'
 				CFDictionaryValueCallBacks callbacks = {0, NULL, NULL, CFCopyDescription, CFEqual};
 
 				sounds_ = (id)CFDictionaryCreateMutable
 					(NULL, (CFIndex)0, &kCFTypeDictionaryKeyCallBacks, &callbacks);
 
-				//----------------------.
-				// Inicializamos OpenAL |
-				//----------------------'
+				//--------------------.
+				// Initialize OpenAL. |
+				//--------------------'
 				const ALCchar *defaultDevice = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
 				ALCdevice *soundDevice = alcOpenDevice(defaultDevice);
 
@@ -129,17 +144,17 @@ static ALCcontext*	    context_ = NULL;
 				alListener3f(AL_ORIENTATION, 0, 0, -1);
 				}
 
-			//---------------------------------------.
-			// Creamos la fuente de sonido de OpenAL |
-			//---------------------------------------'
+			//---------------------------------.
+			// Create the OpenAL sound source. |
+			//---------------------------------'
 			alGenSources(1, &sound->_sourceID);
 			alSource3f(sound->_sourceID, AL_POSITION, 0, 0, 0);
 			alSource3f(sound->_sourceID, AL_VELOCITY, 0, 0, 0);
 			alSourcei (sound->_sourceID, AL_LOOPING, AL_FALSE);
 
-			//---------------------------------------------------------.
-			// Creamos el buffer que contiene los sampels de la fuente |
-			//---------------------------------------------------------'
+			//--------------------------------------------------.
+			// Create the buffer for the samples of the source. |
+			//--------------------------------------------------'
 			alGenBuffers(1, &sound->_bufferID);
 
 			ALenum audioFormat = 0;
@@ -161,14 +176,14 @@ static ALCcontext*	    context_ = NULL;
 				 (ALsizei)(frameCount * description.mBytesPerFrame),
 				 (ALsizei)description.mSampleRate);
 
-			//----------------------------------.
-			// Encolamos el buffer en la fuente |
-			//----------------------------------'
+			//---------------------------------------.
+			// Add the buffer to the source's queue. |
+			//---------------------------------------'
 			alSourceQueueBuffers(sound->_sourceID, 1, &sound->_bufferID);
 
-			//--------------------------------.
-			// Añadimos la instancia al caché |
-			//--------------------------------'
+			//---------------------------------.
+			// Add this instance to the cache. |
+			//---------------------------------'
 			[sounds_ setObject: sound forKey: fileName];
 			}
 
@@ -212,5 +227,3 @@ static ALCcontext*	    context_ = NULL;
 
 
 @end
-
-// EOF
